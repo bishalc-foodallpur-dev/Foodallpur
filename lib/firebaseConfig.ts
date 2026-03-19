aa2c826d3d269bb8de822430f99f83deced41d50
[BIssu Dev] <[bishalc.foodallpur@gmail.com]>
2026-03-20 00:04:09 +0545

    ff

--- lib/firebaseConfig.ts
+++ lib/firebaseConfig.ts
@@ -1,21 +1,21 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, GoogleAuthProvider, FacebookAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Use environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyAj15zSTqBvyqUsWVYRz3tQlIEAniWTMWA",
-  authDomain: "foodallpur-database.firebaseapp.com",
-  projectId: "foodallpur-database",
-  storageBucket: "foodallpur-database.firebasestorage.app",
-  messagingSenderId: "620274381926",
-  appId: "1:620274381926:web:42eecfae77fe51fb0ca9a7",
-  measurementId: "G-YH3W0J93QE"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export const facebookProvider = new FacebookAuthProvider();
+export const db = getFirestore(app);
