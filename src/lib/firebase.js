a9be37d3b0a454e9895b370c99b33b0521f2816e
bissu-dev <bishalc.foodallpur@gmail.com>
2026-03-25 03:54:41 +0545

    mcm

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAj15zSTqBvyqUsWVYRz3tQlIEAniWTMWA",
-  authDomain: "foodallpur-database.firebaseapp.com",
-  projectId: "foodallpur-database",
-  storageBucket: "foodallpur-database.appspot.com",
-  messagingSenderId: "620274381926",
-  appId: "1:620274381926:web:42eecfae77fe51fb0ca9a7",
-  measurementId: "G-YH3W0J93QE"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Prevent re-initialization
